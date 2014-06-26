b9e645c0786ccafd6c190a3de1578256cd6014b3
Mathieu Gagné <mgagne@iweb.com>
2014-06-25 20:22:12 -0400

    Volume type access extension
    This extension adds the ability to manage volume type access:
    * Volume types are public by default
    * Private volume types can be created by setting
      the is_public boolean field to False at creation time.
    * Access to a private volume type can be controlled
      by adding or removing a project from it.
    * Private volume types without projects are only visible
      by users with the admin role/context.
    
    Implementation details and unit tests were mostly adapted
    from Nova flavor access extension.
    
    DocImpact: New volume type access extension
    Implements: blueprint private-volume-types
    Change-Id: I8faf1d8097bf8412d4e169ec3503821351795561
    (cherry picked from commit 6c0f50b1ec933a61b84d806e748afd9cb74e5cd7)

--- cinder/exception.py
+++ cinder/exception.py
@@ -267,6 +267,11 @@ class VolumeTypeNotFoundByName(VolumeTypeNotFound):
                 "could not be found.")
 
 
+class VolumeTypeAccessNotFound(NotFound):
+    message = _("Volume type access not found for %(volume_type_id)s / "
+                "%(project_id)s combination.")
+
+
 class VolumeTypeExtraSpecsNotFound(NotFound):
     message = _("Volume Type %(volume_type_id)s has no extra specs with "
                 "key %(extra_specs_key)s.")
@@ -370,6 +375,11 @@ class Duplicate(CinderException):
 
 class VolumeTypeExists(Duplicate):
     message = _("Volume Type %(id)s already exists.")
+
+
+class VolumeTypeAccessExists(Duplicate):
+    message = _("Volume type access for %(volume_type_id)s / "
+                "%(project_id)s combination already exists.")
 
 
 class VolumeTypeEncryptionExists(Invalid):
