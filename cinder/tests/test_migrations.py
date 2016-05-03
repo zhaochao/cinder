4d096ef637dc4f719f646dfe69550928871efa53
Zhao Chao <zhaochao1984@gmail.com>
2016-05-03 18:18:16 +0800

    unittest: fix test_migration_027.
    Change-Id: I117decfe5ad29d37d0f14ad03c6b0055e92294d7
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
    (cherry picked from commit c6fe1020e750c19445783386ef7d240b99104be7)

--- cinder/tests/test_migrations.py
+++ cinder/tests/test_migrations.py
@@ -1305,11 +1305,11 @@ class TestMigrations(test.TestCase):
             migration_api.version_control(engine,
                                           TestMigrations.REPOSITORY,
                                           migration.db_initial_version())
-            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 31)
+            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 26)
             metadata = sqlalchemy.schema.MetaData()
             metadata.bind = engine
 
-            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 32)
+            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 27)
 
             self.assertTrue(engine.dialect.has_table(engine.connect(),
                                                      "volume_type_projects"))
@@ -1338,7 +1338,7 @@ class TestMigrations(test.TestCase):
             self.assertIsInstance(volume_types.c.is_public.type,
                                   self.bool_type[engine.name])
 
-            migration_api.downgrade(engine, TestMigrations.REPOSITORY, 31)
+            migration_api.downgrade(engine, TestMigrations.REPOSITORY, 26)
             metadata = sqlalchemy.schema.MetaData()
             metadata.bind = engine
 
